b7ad3739962c39cac862138dad0d1977c6cae06b
riecho14 <riecho14@outlook.com>
2023-11-20 21:28:38 +0700

    create filter button

--- app/src/main/java/com/scrum/movieapp/MainActivity.kt
+++ app/src/main/java/com/scrum/movieapp/MainActivity.kt
@@ -2,7 +2,12 @@ package com.scrum.movieapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.annotation.MenuRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,6 +27,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.button2)
+        button.setOnClickListener { v: View ->
+            showMenu(v, R.menu.popup_menu)
+        }
+
         rvFilm = findViewById(R.id.rv_film)
         rvFilm.layoutManager = LinearLayoutManager(this)
         rvFilm.setHasFixedSize(true)
@@ -31,32 +41,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getFilmData() {
-        val database: FirebaseDatabase = FirebaseDatabase.getInstance("https://film-370da-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        val database: FirebaseDatabase =
+            FirebaseDatabase.getInstance("https://film-370da-default-rtdb.asia-southeast1.firebasedatabase.app/")
         dbRef = database.getReference("Film")
 
         Log.d("Database nya", dbRef.database.toString())
 
-        dbRef.addValueEventListener(object: ValueEventListener{
+        dbRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                if(snapshot.exists()){
-                    for(filmSnapshot in snapshot.children){
+                if (snapshot.exists()) {
+                    for (filmSnapshot in snapshot.children) {
                         val film = filmSnapshot.getValue(Film::class.java)
 
-                        Log.d("FirebaseData", "Film name: ${film?.title}, Description: ${film?.description}")
+                        Log.d(
+                            "FirebaseData",
+                            "Film name: ${film?.title}, Description: ${film?.description}"
+                        )
 
                         list.add(film!!)
                     }
                     rvFilm.adapter = ListFilmAdapter(list)
-                }
-                else{
+                } else {
                     Toast.makeText(this@MainActivity, "Data is Null", Toast.LENGTH_SHORT).show()
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Log.w("null_error","data")
+                Log.w("null_error", "data")
                 Toast.makeText(this@MainActivity, "Database Error", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun showMenu(v: View, @MenuRes menuRes: Int) {
+        val popup = PopupMenu(v.context, v)
+        popup.menuInflater.inflate(menuRes, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            // Respond to menu item click.
+            true
+        }
+        popup.setOnDismissListener {
+            // Respond to popup being dismissed.
+        }
+        popup.show()
     }
 }
