1507dac472d0301e408e05f26622130bf462edb8
riecho14 <riecho14@outlook.com>
2023-11-20 20:22:26 +0700

    add price

--- app/src/main/java/com/scrum/movieapp/DetailActivity.kt
+++ app/src/main/java/com/scrum/movieapp/DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.scrum.movieapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.scrum.movieapp.databinding.ActivityDetailBinding
@@ -17,11 +18,18 @@ class DetailActivity : AppCompatActivity() {
 
         if (film != null) {
             binding.judulFilm.text = film.title
+            binding.buyFilm.text = film.title
             binding.genreFilm.text = film.genre
             binding.pemainFilm.text = film.actors
             binding.deskripsiFilm.text = film.description
+            binding.hargaFilm.text = film.price
 
             Glide.with(this).load(film.image).into(binding.imageView2)
+        }
+
+        binding.hargaFilm.setOnClickListener {
+            Toast.makeText(this, "Pembayaran berhasil", Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
